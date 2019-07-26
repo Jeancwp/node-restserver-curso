f5df38c86b6834e8f1b4beab6dcb6dea2823736c
Jean <jeancwp@hotmail.com>
2019-07-25 22:03:52 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //  Puerto 
 //============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//============================
+//  Entorno 
+//============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//============================
+// Base de Datos
+//============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/menu';
+} else {
+    urlDB = 'mongodb+srv://jeancwp:Jeanclaude100@cluster0-qe8wd.mongodb.net/menu';
+}
+process.env.URLDB = urlDB;
